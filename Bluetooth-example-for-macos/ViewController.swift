f086fcc8d79ee13a43ea7ce63280ec06bf80a859
yuu <yuuxzy@gmail.com>
2019-11-05 12:48:43 +0900

    Initial Commit

---
+++ Bluetooth-example-for-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bluetooth-example-for-macos
+//
+//  Created by 14-0254 on 2019/11/05.
+//  Copyright © 2019 Yusuke Binsaki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
